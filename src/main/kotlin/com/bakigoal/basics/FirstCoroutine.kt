b1e926f423e71d6ff0a02b6bb3e55f13b535bd3d
Ilmir Bakirov <ilmirbakirov@gmail.com>
2022-03-04 21:11:20 +0500

    extract doWorld as suspend function

--- src/main/kotlin/com/bakigoal/basics/FirstCoroutine.kt
+++ src/main/kotlin/com/bakigoal/basics/FirstCoroutine.kt
@@ -6,17 +6,18 @@ import kotlinx.coroutines.runBlocking
 import kotlin.time.DurationUnit
 import kotlin.time.toDuration
 
-fun main() {
-    runBlocking {      // this: CoroutineScope
-        launch {       // launch a new coroutine and continue
-            // delay is a special suspending function.
-            // It suspends the coroutine for a specific time.
-            // Suspending a coroutine does not block the underlying thread,
-            // but allows other coroutines to run and use the underlying thread for their code.
-            delay(1.toDuration(DurationUnit.SECONDS))
-            println("World!")        // print after delay
-        }
-        println("Hello")        // main coroutine continues while a previous one is delayed
+fun main() = runBlocking {      // this: CoroutineScope
+    launch {                // launch a new coroutine and continue
+        doWorld()
     }
-    println("The End!!!")
+    println("Hello")        // main coroutine continues while a previous one is delayed
+}
+
+private suspend fun doWorld() {
+    // delay is a special suspending function.
+    // It suspends the coroutine for a specific time.
+    // Suspending a coroutine does not block the underlying thread,
+    // but allows other coroutines to run and use the underlying thread for their code.
+    delay(1.toDuration(DurationUnit.SECONDS))
+    println("World!")        // print after delay
 }
