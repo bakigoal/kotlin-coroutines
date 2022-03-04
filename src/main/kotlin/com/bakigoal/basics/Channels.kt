425c7e2029b441272a77761880d21b51f2192fb5
Ilmir Bakirov <ilmirbakirov@gmail.com>
2022-03-04 22:39:29 +0500

    add dispatchers

--- src/main/kotlin/com/bakigoal/basics/Channels.kt
+++ src/main/kotlin/com/bakigoal/basics/Channels.kt
@@ -1,5 +1,6 @@
 package com.bakigoal.basics
 
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
@@ -7,18 +8,18 @@ import kotlinx.coroutines.runBlocking
 fun main() = runBlocking<Unit> {
     val channel = Channel<String>()
 
-    launch() {
+    launch(Dispatchers.Default) {
         channel.send("A1")
         channel.send("A2")
         log("A - done")
     }
 
-    launch {
+    launch(Dispatchers.Default) {
         channel.send("B1")
         log("B - done")
     }
 
-    launch {
+    launch(Dispatchers.Default) {
         repeat(3) {
             log(channel.receive())
         }
