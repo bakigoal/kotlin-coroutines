36380112d34851869a1f23af17952bbb54e1aad2
Ilmir Bakirov <ilmirbakirov@gmail.com>
2022-03-04 21:55:48 +0500

    wait all deferred functions

--- src/main/kotlin/com/bakigoal/basics/AsyncDeferred.kt
+++ src/main/kotlin/com/bakigoal/basics/AsyncDeferred.kt
@@ -1,21 +1,18 @@
 package com.bakigoal.basics
 
 import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
-    val deferred = async {
-        loadData()
+    val deferred = (1..3).map {
+        async {
+            delay(1000L * it)
+            println("Loading $it")
+            it
+        }
     }
     println("waiting...")
-    println(deferred.await())
-}
-
-suspend fun loadData(): Int {
-    delay(1000L)
-    println("loading...")
-    delay(3000L)
-    println("loaded!")
-    return 42
+    println(deferred.awaitAll().sum())
 }
