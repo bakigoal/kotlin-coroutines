1e9dd6df3deb0bf839bda14b62765a12d7686b9b
Ilmir Bakirov <ilmirbakirov@gmail.com>
2022-03-04 23:26:46 +0500

    add coroutine flow

--- src/main/kotlin/com/bakigoal/flow/AsyncFlow.kt
+++ src/main/kotlin/com/bakigoal/flow/AsyncFlow.kt
@@ -1,12 +1,25 @@
 package com.bakigoal.flow
 
-fun simple(): Sequence<Int> = sequence { // sequence builder
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
+
+fun simple1(): Sequence<Int> = sequence { // sequence builder
     for (i in 1..5) {
-        Thread.sleep(500) // pretend we are computing it
+        Thread.sleep(1000) // pretend we are computing it
         yield(i) // yield next value
     }
 }
 
-fun main() {
-    simple().forEach { value -> println(value) }
+suspend fun simple(): Flow<Int> = flow {
+    for (i in 1..5) {
+        delay(1000) // pretend we are doing something asynchronous here
+        emit(i) // yield next value
+    }
+}
+
+fun main() = runBlocking {
+    simple1().forEach { println("Seq $it") }
+    simple().collect { println("Flow $it") }
 }
