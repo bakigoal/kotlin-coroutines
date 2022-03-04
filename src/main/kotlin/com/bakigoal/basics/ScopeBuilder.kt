d75592c909a8c59c1774d678fe4b84d11869dd37
Ilmir Bakirov <ilmirbakirov@gmail.com>
2022-03-04 21:16:30 +0500

    custom coroutine scope

---
+++ src/main/kotlin/com/bakigoal/basics/ScopeBuilder.kt
@@ -0,0 +1,18 @@
+package com.bakigoal.basics
+
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+    doWorld()
+}
+
+private suspend fun doWorld() = coroutineScope {
+    launch {
+        delay(1000L)
+        println("World!")
+    }
+    println("Hello")
+}
