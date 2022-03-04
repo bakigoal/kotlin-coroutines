9cc8e95d253e96ac439659b3a999a18f8560b0e7
Ilmir Bakirov <ilmirbakirov@gmail.com>
2022-03-04 21:07:25 +0500

    initial commit

--- src/main/kotlin/com/bakigoal/basics/FirstCoroutine.kt
+++ src/main/kotlin/com/bakigoal/basics/FirstCoroutine.kt
@@ -9,9 +9,14 @@ import kotlin.time.toDuration
 fun main() {
     runBlocking {      // this: CoroutineScope
         launch {       // launch a new coroutine and continue
-            delay(1.toDuration(DurationUnit.SECONDS))    // non-blocking delay for 1 second
+            // delay is a special suspending function.
+            // It suspends the coroutine for a specific time.
+            // Suspending a coroutine does not block the underlying thread,
+            // but allows other coroutines to run and use the underlying thread for their code.
+            delay(1.toDuration(DurationUnit.SECONDS))
             println("World!")        // print after delay
         }
         println("Hello")        // main coroutine continues while a previous one is delayed
     }
+    println("The End!!!")
 }
